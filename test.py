331f672d326ca540f4e0a0f1b7f2749f6f62d5eb
Yuxuan <yuxuan_zhang13@outlook.com>
2024-11-13 00:46:19 -0800

    wrap up werewolf program

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from game import GameSession
 import json
-with open("/Users/jasondai/Desktop/UCSD/DSC 190 MLFL/RWWW-Reasoning-with-Werewolf/config/gpt-4o-mini-game-config.json", "r") as file:  # Replace "config.json" with your file path
+with open("./config/gpt-4o-mini-game-config.json", "r") as file:  # Replace "config.json" with your file path
     config = json.load(file)
  
 
@@ -8,4 +8,4 @@ game = GameSession(config)
 game.display_player_roles()
 game.initialize_agent_response()
 
-game.play_round()
+game.play_rounds()
